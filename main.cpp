a825a1543498ee32c6020128dfe4c56f4a334499
Alex <alexlin@weintek.com>
2022-07-12 03:18:48 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,158 @@
+#include <string>
+#include <stack>
+#include <deque>
+#include <iostream>
+#include <sstream>
+
+//#define GTEST
+typedef std::vector< std::deque< int > > State;
+class Game
+{
+public:
+    Game(std::deque< int > &hand)
+        : hand_(hand)
+    {
+        init();
+    };
+
+public:
+    void init();
+    bool record();
+    bool check(std::deque< int > &);
+    int place(int &pos);
+    std::string play();
+
+private:
+    std::deque< int > hand_;
+    std::deque< int > pile_[7];
+    std::map< State, bool > hand_record;
+};
+
+void Game::init()
+{
+    for (int i = 0; i < 7; i++)
+        pile_[i].push_back(hand_.front()), hand_.pop_front();
+    record();
+}
+
+bool Game::record()
+{
+    State tmp;
+    tmp.push_back(hand_);
+    for (int i = 0; i < 7; i++)
+        tmp.push_back(pile_[i]);
+    if (hand_record[tmp])
+        return 0;
+    hand_record[tmp] = 1;
+    return 1;
+}
+
+bool Game::check(std::deque< int > &q)
+{
+    if (q.size() < 3)
+        return 0;
+    int first = q.front(), last = q.back();
+    q.pop_front(), q.pop_back();
+    if (first + q.front() + last == 10 || first + q.front() + last == 20 || first + q.front() + last == 30) {
+        hand_.push_back(first);
+        hand_.push_back(q.front());
+        hand_.push_back(last);
+        q.pop_front();
+        return 1;
+    }
+    if (first + q.back() + last == 10 || first + q.back() + last == 20 || first + q.back() + last == 30) {
+        hand_.push_back(first);
+        hand_.push_back(q.back());
+        hand_.push_back(last);
+        q.pop_back();
+        return 1;
+    }
+    q.push_front(first);
+    first = q.back();
+    q.pop_back();
+
+    if (first + q.back() + last == 10 || first + q.back() + last == 20 || first + q.back() + last == 30) {
+        hand_.push_back(q.back());
+        hand_.push_back(first);
+        hand_.push_back(last);
+        q.pop_back();
+        return 1;
+    }
+    q.push_back(first), q.push_back(last);
+    return 0;
+}
+int Game::place(int &pos)
+{
+    while (pile_[pos].empty())
+        pos = (pos + 1) % 7;
+    pile_[pos].push_back(hand_.front()), hand_.pop_front();
+    if (!record())
+        return -1;
+    if (pile_[pos].size() > 2)
+        while (check(pile_[pos]))
+            ;
+
+    if (hand_.empty())
+        return 0;
+
+    return 1;
+}
+
+std::string Game::play()
+{
+    std::string result;
+    int times = 7, pos = 0;
+    while (1) {
+
+        if (hand_.size() == 52) {
+            result = "Win : ";
+            break;
+        }
+        int c = place(pos);
+
+        if (c == -1) {
+            result = "Draw: ";
+            break;
+        } else if (!c) {
+            times++;
+            result = "Loss: ";
+            break;
+        }
+        ++times, pos = (pos + 1) % 7;
+    }
+    result += std::to_string(times);
+
+    result += "\n";
+    return result;
+}
+
+void uva246(std::istream &is, std::ostream &os)
+{
+    int number;
+    while ((is >> number) && number) {
+        std::deque< int > hand;
+        hand.push_back(number);
+
+        for (int i = 0; i < 51; i++) {
+            is >> number;
+            hand.push_back(number);
+        }
+
+        Game game(hand);
+        os << game.play();
+
+        hand.clear();
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+#ifdef GTEST
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+#else
+    uva246(std::cin, std::cout);
+#endif
+    return 0;
+}
